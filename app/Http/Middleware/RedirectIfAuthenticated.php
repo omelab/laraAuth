91f1d3551f2ff99e9eadd9ee5a95eb8f5c3d4a37
Abu Bakar Siddique <abu.bakar@apsissolutions.com>
2019-11-07 13:33:43 +0600

    some of chage on acl

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if ($guard == "admin" && Auth::guard($guard)->check()) {
-            return redirect('/admin');
+            return redirect('/lara-admin');
         }
         if ($guard == "customer" && Auth::guard($guard)->check()) {
             return redirect('/customer');
